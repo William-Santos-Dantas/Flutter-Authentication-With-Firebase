e696e8d3b9cf467598ebff48a3a1b5fdb5cba14a
unknown <NidrahDev@protonmail.com>
2021-03-12 19:39:30 -0300

    first commit

---
+++ android/app/src/main/kotlin/nidrah/authentication_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package nidrah.authentication_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './constants/app_theme.dart';
+import './routes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Authentication FireBase',
+      theme: themeData,
+      initialRoute: '/',
+      routes: Routes.routes,
+    );
+  }
+}
